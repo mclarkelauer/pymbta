57b2943883bd7ecb22a4b28dc5683f4f56b449b8
Matt Clarke-Lauer <matt@clarkelauer.com>
2014-12-16 10:11:58 -0500

    add *.swo to gitignore

--- mbta/test.py
+++ mbta/test.py
@@ -8,12 +8,16 @@ class baseTestCase(unittest.TestCase):
   def setUp(self):
     self.mbtaHandle = mbta.mbta(apiKey)
 
-class badKeyTestCase(unittest.TestCase):
+class badKeyTest(unittest.TestCase):
   def setUp(self):
     self.mbtaHandle = mbta.mbta("")
 
   def test_badApiKey(self):
-    self.assertRaises(urllib2.HTTPError,self.mbtaHandle.routes())
+    self.assertRaises(urllib2.HTTPError,lambda: self.mbtaHandle.routes())
+
+class singleRequestTests(baseTestCase):
+  def test_routes(self):
+    
 
 if __name__ == '__main__':
   unittest.main()
